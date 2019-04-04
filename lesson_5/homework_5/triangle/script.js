692b02fca53294f8e0c93add30f60691a249482f
wrublik93 <wrublik.93@mail.ru>
2019-04-04 12:45:58 +0300

    complete triangle

--- lesson_5/homework_5/triangle/script.js
+++ lesson_5/homework_5/triangle/script.js
@@ -1,30 +1,32 @@
-function triangle(x,y,z) {
-    var arr = [x,y,z];
+var x = document.getElementById('sideA').value;
+var y = document.getElementById('sideB').value;
+var z = document.getElementById('sideC').value;
+
+function triangle(x, y, z) {
+    x = parseInt(document.getElementById('sideA').value);
+    y = parseInt(document.getElementById('sideB').value);
+    z = parseInt(document.getElementById('sideC').value);
+
+    let arr = [x,y,z];
     function compareNumeric(m, n) {
         if (m > n) return 1;
         if (m < n) return -1;
     }
     arr.sort(compareNumeric);
-    console.log('Массив отсортированных чисел: '+ arr.sort(compareNumeric));
-    var a = arr[0];
-    var b = arr[1];
-    var c = arr[2];
-    var sum = Math.pow(a,2)+ Math.pow(b,2);
-    var big = Math.pow(c, 2);
+    let a = arr[0];
+    let b = arr[1];
+    let c = arr[2];
+    let sum = Math.pow(a,2)+ Math.pow(b,2);
+    let big = Math.pow(c, 2);
     if(((a+b)<=c)||((a+c)<=b)||((b+c)<=a)) {
-        console.log('не существует')
+        console.log('Треугольник не существует')
     } else {
         if (sum === big) {
-            console.log('прямоугольный')
+            console.log('Треугольник прямоугольный')
         } else if (sum < big) {
-            console.log('тупоугольный')
+            console.log('Треугольник тупоугольный')
         } else if (sum > big) {
-            console.log('остроугольный')
+            console.log('Треугольник остроугольный')
         }
     }
 }
-
-console.log(triangle(4,6,2));
-console.log(triangle(14,10,8));
-console.log(triangle(9,8,10));
-console.log(triangle(8,6,10));
